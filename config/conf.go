e2a805fe555267880a812e41283deed0a64b99ef
Quinn T <Quinn.GitHub@gmail.com>
2021-06-03 18:25:14 -0500

    Merge files

--- config/conf.go
+++ config/conf.go
@@ -29,3 +29,30 @@ func GetPublic() (*PublicConfig, error) {
 
 	return &config, nil
 }
+
+// Handling private config
+
+type PrivateConfig struct {
+	// Even though the username is not 'private' its very much related so I will keep it here
+
+	// Username of account
+	Username string `json:"username"`
+	// Oauth token of account
+	Oauth string `json:"oauth"`
+}
+
+func GetPrivate() (conf *PrivateConfig, err error) {
+	bytes, err := ioutil.ReadFile("./config/private_conf.json")
+	if err != nil {
+		return nil, err
+	}
+
+	var config PrivateConfig
+	err = json.Unmarshal(bytes, &config)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
