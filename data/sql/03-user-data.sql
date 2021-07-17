bde02fe6a3d6d646096063bb9ac92b87f0a8bc6e
Quinn T <Quinn.GitHub@gmail.com>
2021-07-17 12:37:12 -0500

    Reformat SQL

--- data/sql/03-user-data.sql
+++ data/sql/03-user-data.sql
@@ -1,2 +1,5 @@
-INSERT INTO `wb_core`.`user` (`ID`, `Name`, `Twitch_ID`, `First_Seen`) VALUES (1, 'quinndt', 440674731, '2020-06-24 06:01:01');
-INSERT INTO `wb_core`.`user` (`ID`, `Name`, `Twitch_ID`, `First_Seen`) VALUES (2, 'turtoise', 485693696, '2020-06-24 06:01:01');
+INSERT INTO `wb_core`.`user` (`ID`, `Name`, `Twitch_ID`, `First_Seen`)
+VALUES
+    (1, 'quinndt', 440674731, '2020-06-24 06:01:01'),
+    (2, 'turtoise', 485693696, '2020-06-24 06:01:01')
+;
