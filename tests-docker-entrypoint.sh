871fb54a73fc8a9195b213a1fcc280c68d9f1911
Quinn T <Quinn.GitHub@gmail.com>
2021-07-16 21:08:36 -0500

    Update entrypoint to exit on any failed command

--- tests-docker-entrypoint.sh
+++ tests-docker-entrypoint.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
+
 # I dont know if mariadb will always be working before this starts.
 sleep 3
 # I dont know if WORKDIR is preserved when executing the entrypoint.
 cd /bot
 # Populate the database with test data.
-bin/populator
+/bot/bin/populator
 # Run all tests.
 go test ./...
