6bb3ceccbd8634475e36709334e38f5b90f4b9c4
Quinn T <Quinn.GitHub@gmail.com>
2021-08-19 19:14:57 -0500

    Update user table SQL definition

--- data/sql/02-user.sql
+++ data/sql/02-user.sql
@@ -3,7 +3,9 @@ CREATE TABLE IF NOT EXISTS `wb_core`.`user` (
 	`Name` VARCHAR(50) NOT NULL COLLATE 'utf8_general_ci',
 	`Twitch_ID` INT(16) UNSIGNED NOT NULL,
 	`First_Seen` DATETIME NOT NULL DEFAULT current_timestamp() COMMENT 'When user was first added to the database',
-	PRIMARY KEY (`ID`) USING BTREE
+	PRIMARY KEY (`ID`) USING BTREE,
+	UNIQUE INDEX `Name` (`Name`) USING BTREE,
+	UNIQUE INDEX `Twitch_ID` (`Twitch_ID`) USING BTREE
 )
 COMMENT='Users stored in the system'
 COLLATE='utf8_general_ci'
